9944fd7b03b272c6fc0c4c37bf3e65e749672965
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2019-10-12 00:24:47 +0200

    Added base terraform files for deploying resource group and kubernetes cluster

---
+++ infra/terraform.tfvars
@@ -0,0 +1,2 @@
+prefix="SysTest"
+location="West Europe"
---
+++ infra/variables.tf
@@ -0,0 +1,8 @@
+variable "prefix" {
+  description = "A prefix used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be provisioned"
+}
+
