00e7d3af1928643c22ae119d8f013e8c2331d785
Vicente Herrera <vicente.herrera@sngular.com>
2019-10-22 05:04:57 +0200

    Better echo message

--- infra/load_from_terraform.sh
+++ infra/load_from_terraform.sh
@@ -10,7 +10,7 @@ echo "Reading ACR URI"
 export ACR_URI="$(terraform output acr_uri)"
 export SKAFFOLD_DEFAULT_REPO=$ACR_URI
 
-echo "Login into Docker"
+echo "Login with Docker to your ACR"
 docker login $ACR_URI -u $(terraform output acr_user) -p $(terraform output acr_password)
 
 cd $dir_original
